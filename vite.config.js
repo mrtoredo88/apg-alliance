90a8450a42cbcf12200a88c90465be52da085a15
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 13:53:54 +0300

    clean config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Собираем строго в папку build
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
-    className: true,
+    outDir: 'build', // На всякий случай оставляем сборку в папку build
   },
 });
