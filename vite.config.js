488312851a32a69f96e9218dc08c7b2223bb52d6
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 19:19:16 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  // Удаляем base: './', пусть Vite сам определяет пути при сборке
 })
