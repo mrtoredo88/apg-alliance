7c3c25b7b8f0ce5e319ff90bba4940106d6a5a59
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 21:35:43 +0300

    Update AppConfig.jsx

--- src/AppConfig.jsx
+++ src/AppConfig.jsx
@@ -1,31 +1,26 @@
-import vkBridge, { parseURLSearchParamsForGetLaunchParams } from '@vkontakte/vk-bridge';
-import { useAdaptivity, useAppearance, useInsets } from '@vkontakte/vk-bridge-react';
-import { AdaptivityProvider, ConfigProvider, AppRoot } from '@vkontakte/vkui';
-import { RouterProvider } from '@vkontakte/vk-mini-apps-router';
-import '@vkontakte/vkui/dist/vkui.css';
-
-import { transformVKBridgeAdaptivity } from './utils';
-import { router } from './routes';
-import { App } from './App';
+import { AdaptivityProvider, ConfigProvider, AppRoot, SplitLayout, SplitCol, View, Panel, PanelHeader, Group, Placeholder } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css'; // Обязательно импортируем стили!
 
 export const AppConfig = () => {
-  const vkBridgeAppearance = useAppearance() || undefined;
-  const vkBridgeInsets = useInsets() || undefined;
-  const adaptivity = transformVKBridgeAdaptivity(useAdaptivity());
-  const { vk_platform } = parseURLSearchParamsForGetLaunchParams(window.location.search);
-
   return (
-    <ConfigProvider
-      colorScheme={vkBridgeAppearance}
-      platform={vk_platform === 'desktop_web' ? 'vkcom' : undefined}
-      isWebView={vkBridge.isWebView()}
-      hasCustomPanelHeaderAfter={true}
-    >
-      <AdaptivityProvider {...adaptivity}>
-        <AppRoot mode="full" safeAreaInsets={vkBridgeInsets}>
-          <RouterProvider router={router}>
-            <App />
-          </RouterProvider>
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <AppRoot>
+          <SplitLayout header={<PanelHeader separator={false} />}>
+            <SplitCol>
+              <View activePanel="main">
+                <Panel id="main">
+                  <PanelHeader>APG Alliance</PanelHeader>
+                  <Group>
+                    {/* Сюда мы будем добавлять твои компоненты */}
+                    <Placeholder>
+                      Дизайн в процессе настройки!
+                    </Placeholder>
+                  </Group>
+                </Panel>
+              </View>
+            </SplitCol>
+          </SplitLayout>
         </AppRoot>
       </AdaptivityProvider>
     </ConfigProvider>
