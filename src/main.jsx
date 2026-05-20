ca58bc14d2b686ada2b7ffca58aaeddae1505081
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 19:59:35 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
-import { AppConfig } from './AppConfig.js';
+import { AppConfig } from './AppConfig.jsx';
 
 vkBridge.send('VKWebAppInit');
 
