153e9f2d39d469976541e06ba124d67834e9755d
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 14:01:41 +0300

    final build fix
    final build fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // На всякий случай оставляем сборку в папку build
+    outDir: 'build',
+  },
+  // Этот блок принудительно заставляет Vite читать JSX внутри .js файлов,
+  // даже если сервер Vercel пытается капризничать
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
   },
 });
