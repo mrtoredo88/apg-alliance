ad9c991d90a8cb3b35214bea3309bbafe432e9ab
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 16:45:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Scanner } from './Scanner';
+import { Scanner } from './Scanner.jsx';
 
 export function App() {
   // База данных ключей
