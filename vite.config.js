c60f8a1a878328224d56a61eea68ee27e058ddf1
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-20 13:13:37 +0300

    Update vite.config.js
    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,62 +1,10 @@
-import { defineConfig, transformWithEsbuild } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 
-function handleModuleDirectivesPlugin() {
-  return {
-    name: 'handle-module-directives-plugin',
-    transform(code, id) {
-      if (id.includes('@vkontakte/icons')) {
-        code = code.replace(/"use-client";?/g, '');
-      }
-      return { code };
-    },
-  };
-}
-
-function threatJsFilesAsJsx() {
-  return {
-    name: 'treat-js-files-as-jsx',
-    async transform(code, id) {
-      if (!id.match(/src\/.*\.js$/)) return null;
-
-      return transformWithEsbuild(code, id, {
-        loader: 'jsx',
-        jsx: 'automatic',
-      });
-    },
-  };
-}
-
-/**
- * Some chunks may be large.
- * This will not affect the loading speed of the site.
- * We collect several versions of scripts that are applied depending on the browser version.
- * This is done so that your code runs equally well on the site and in the odr.
- * The details are here: https://dev.vk.ru/mini-apps/development/on-demand-resources.
- */
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-
-  plugins: [
-    react(),
-    threatJsFilesAsJsx(),
-    handleModuleDirectivesPlugin(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
-
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-
+  plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist', // Принудительно заставляем собирать в папку dist
   },
 });
