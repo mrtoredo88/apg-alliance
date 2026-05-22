15f26ee27b7c1ea560677c8af5c77cd5883f4ff9
mrtoredo88 <104310420+mrtoredo88@users.noreply.github.com>
2026-05-22 10:58:45 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
 import { App } from './App.jsx'; // Рендерим именно Роутер!
+import './index.css'; // Добавьте это в начало файла
 
 vkBridge.send('VKWebAppInit');
 
